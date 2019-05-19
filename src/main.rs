ba0a414a5e3d059061842b91ab48c7ab854c33c5
desmonty <desmonty4620@gmail.com>
2019-05-19 13:25:46 -0400

    use iterators

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,18 @@ mod approx_fraction;
 use approx_fraction::*;
 
 fn main() {
-    let num = 23;
+    let num = 3;
+    let len = 63;
 
-    for i in 2..62 {
-        let before_approx = approximate_fraction(1, num, i-1);
-        let approx = approximate_fraction(1, num, i);
-        println!("{:?} ::: {:?} ::: {:?}", (approx.0) as i64 - 2*(before_approx.0) as i64, approx.0 as f64 / approx.1 as f64, approx);
+    let vec_approx: Vec<_> = (1..len+1).map(|i| approximate_fraction(1, num, i)).collect();
+
+    let vec_iter = vec_approx.iter()
+        .enumerate()
+        .filter(|x| x.0 < (len-1) as usize);
+
+    for (i, val) in vec_iter {
+        let before_approx = val;
+        let approx = vec_approx[i+1];
+        println!("{:?} ::: {:?} ::: {:?}", approx.0 as i64 - 2*before_approx.0 as i64, approx.0 as f64 / approx.1 as f64, approx);
     }
 }
